df31672cfce6c9f940ea9a3800eddea1c468ce7b
Dhary-Anne <ddavilmar2014@fau.edu>
2022-03-02 16:20:33 -0500

    fixed bg img

--- webpack.config.js
+++ webpack.config.js
@@ -12,12 +12,6 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
-      {
-        test: /\.(jpg|png)$/,
-        use: {
-          loader: 'url-loader',
-        },
-      },
     ],
   },
 };
